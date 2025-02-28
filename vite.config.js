67e06f61bd62b74fb58570b4a2b2d7930a30ee77
projectblazes <gene.stick.fruit@cloak.id>
2025-02-28 16:43:46 -0500

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/projectblaze/', // This should match your repository name
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+  }
+}) 
